e6f93c5bacca2c712256a6be630a34f515cbdacf
baluborsh <pandapearbalu@gmail.com>
2020-02-18 23:07:10 +0300

    another version

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,25 @@ var bstone = document.getElementById('b-stone');
 var bscis = document.getElementById('b-scis');
 var bpaper = document.getElementById('b-paper');
 var field = document.getElementById('field');
+var userButton = document.getElementById('changeuser');
+var userHeading = document.getElementById('greetings');
+
+function setUserName() {
+   var myName = prompt('Пожалуйста представьтесь');
+   localStorage.setItem('name', myName);
+   userHeading.textContent = 'Приветствую, ' + myName + '!';
+}
+
+if(!localStorage.getItem('name')) {
+   setUserName();
+} else {
+   var storedName = localStorage.getItem('name');
+   userHeading.textContent = 'Приветствую, ' + storedName + '!';
+}
+
+userButton.onclick = function() {
+   setUserName();
+}
 
 function stone() {
    document.write("<h2>Вы могли выбрать что-нибудь другое...</h2> <p>Но вы выбрали камень, поэтому видите эту страницу</p> <input type='button' value='Вас понял...' onClick='window.location.reload()'>");
